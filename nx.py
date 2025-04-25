4d95cc04f344ecce007815b1f98b927cde6b65db
Rebecca Salganik <rebeccasalganik@vpn-10-40-8-42.net.rochester.edu>
2025-04-25 19:28:33 -0400

    final version

--- nx.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx 
-import numpy as np 
-import ipdb 
-
-degrees = [0, 1, 2, 2, 2, 2, 3]
-adj = np.load('/Users/rebeccasalganik/Documents/School/2025/Distributed/Data/Karate.npy')
-H = nx.from_numpy_array(adj) #nx.havel_hakimi_graph(degrees)
-print(nx.k_core(H).nodes) 
